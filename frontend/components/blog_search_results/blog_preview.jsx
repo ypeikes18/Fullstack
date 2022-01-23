bd25e67cdcbeb367a2af083836e5d80367dab523
ypeikes18 <ypeikes18@gmail.com>
2022-01-23 12:12:40 -0500

    some styling

--- frontend/components/blog_search_results/blog_preview.jsx
+++ frontend/components/blog_search_results/blog_preview.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BlogPreview = ({ blog }) => {
-    debugger
-    const { title, 
+    const { id,
+        title, 
         author, 
         icon_url, 
         description } = blog;
 
     return(
-        <div className='blog-preview-container'>
-            <img 
-            src={icon_url}
-            className='blog-preview-image'/>
-            <div className='blog-preview-text'>
-                <h1>{title}</h1>
-                <h3>{description}</h3>
-                <span>{`By #${author}`}</span>
+        <Link to={`/blogs/${id}`}>
+            <div 
+            className='blog-preview-container'>
+                <img 
+                src={icon_url}
+                className='blog-preview-image'/>
+                <div className='blog-preview-text'>
+                    <h1>{title}</h1>
+                    <h3>{description}</h3>
+                    <span>{`By #${author}`}</span>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
